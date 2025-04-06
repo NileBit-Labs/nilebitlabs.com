507d3939832027154045cdd101b7f4fd13fef274
douglasbagambe <douglasbagambe4@gmail.com>
2025-04-07 00:12:14 +0300

    Initial app/page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-import { HomeHero } from '../components/sections/HomeHero';
+import Hero from "../components/ui/Hero";
+import ServicesSection from "../components/sections/ServicesSection";
 
 export default function Home() {
   return (
-    <main>
-      <HomeHero />
-      {/* Add more sections here */}
-    </main>
+    <div>
+      <Hero
+        title="Empowering Innovation with NileBit Labs"
+        subtitle="Cutting-Edge Software Solutions for the Future"
+        ctaText="Discover Our Solutions"
+        ctaLink="/solutions"
+      />
+      <ServicesSection />
+    </div>
   );
 }
