428a83569c25680b9537804b55e265884b0a890b
douglasbagambe <douglasbagambe4@gmail.com>
2025-04-07 00:11:40 +0300

    Initial MainLayout

---
+++ components/layout/MainLayout.tsx
@@ -0,0 +1,16 @@
+import Navbar from "../ui/Navbar";
+import Footer from "../ui/Footer";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow pt-16">{children}</main>
+      <Footer />
+    </div>
+  );
+}
