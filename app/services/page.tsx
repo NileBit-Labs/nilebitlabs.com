7cd61a6aaf8e26e77b7e5a0408e06e2710f76cbf
SHEMA_COLLINS <shemashetou7@gmail.com>
2025-04-08 16:43:45 +0300

    Updated services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -6,7 +6,7 @@ export default function Services() {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className="p-6 bg-gray-50 rounded-lg shadow-md">
             <h2 className="text-2xl font-semibold text-orange-500 mb-4">
-              01 Custom Software Development
+              01 Fullstack Software Development
             </h2>
             <p className="text-gray-600">
               We build bespoke software solutions tailored to your unique
@@ -15,7 +15,7 @@ export default function Services() {
           </div>
           <div className="p-6 bg-gray-50 rounded-lg shadow-md">
             <h2 className="text-2xl font-semibold text-orange-500 mb-4">
-              02 AI & Machine Learning
+              02 AI Agents Solutions, AI & Machine Learning
             </h2>
             <p className="text-gray-600">
               Harness the power of AI to drive innovation and efficiency in your
@@ -37,6 +37,22 @@ export default function Services() {
             </h2>
             <p className="text-gray-600">
               Protect your business with our advanced cybersecurity services.
+            </p>
+          </div>
+          <div className="p-6 bg-gray-50 rounded-lg shadow-md">
+            <h2 className="text-2xl font-semibold text-orange-500 mb-4">
+              05 Web3 & Blockchain dAapp Development
+            </h2>
+            <p className="text-gray-600">
+            Welcome to the future of the internet – build and deploy decentralized applications..
+            </p>
+          </div>
+          <div className="p-6 bg-gray-50 rounded-lg shadow-md">
+            <h2 className="text-2xl font-semibold text-orange-500 mb-4">
+              06 Fintech Solutions
+            </h2>
+            <p className="text-gray-600">
+            Where finance meets innovation – scalable tech for modern financial services.
             </p>
           </div>
         </div>
